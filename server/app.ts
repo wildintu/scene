0aafc4aa43cbf2f28e0bde82a4529e050fe249b8
wildintu <lexi.kwak@unosquare.com>
2024-08-02 11:49:43 -0500

    Connect with Express and Docker. Not able to initialize PrismaClient on local host 5435 or 5432

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient();
 const app = express();
 
 // app.use(cors());
@@ -8,7 +10,15 @@ const app = express();
 // app.use(express.json());
 // app.use(morgan('combined'));
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
+  const allClubs = await prisma.club.findMany()
+  const allVenues = await prisma.venue.findMany()
+  const allEvents = await prisma.event.findMany()
+  const allAttendees = await prisma.attendee.findMany()
+  console.log(allClubs)
+  console.log(allVenues)
+  console.log(allEvents)
+  console.log(allAttendees)
   res.send('Scene - Club Event App - Home')
 });
 
